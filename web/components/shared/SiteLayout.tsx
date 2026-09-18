f2a59cc886b21ed60dfa0d29c3273c9c99153c74
森雄大 <forestocean0119@gmail.com>
2026-09-18 20:26:54 +0900

    Refine shared public page layout

--- web/components/shared/SiteLayout.tsx
+++ web/components/shared/SiteLayout.tsx
@@ -1,5 +1,6 @@
 import SiteHeader from "./SiteHeader";
 import SiteFooter from "./SiteFooter";
+import s from "./shared.module.css";
 
 // 公開サイトの入口は、撮影先・購入者・データ運用の三者に固定する。
 export default async function SiteLayout({
@@ -8,10 +9,10 @@ export default async function SiteLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className={s.siteLayout}>
       <SiteHeader />
       {children}
       <SiteFooter />
-    </>
+    </div>
   );
 }
